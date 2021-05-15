6f904ebd0d6c5854bcebbdfa956d210d3ecf0662
wassimyaakoubi <81557472+wassimyaakoubi@users.noreply.github.com>
2021-05-15 10:17:03 +0100

    Delete MontDePiete-Wassim directory

--- MontDePiete-Wassim/src/main/java/tn/esprit/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import tn.esprit.entities.Account;
-
-public interface AccountRepository extends CrudRepository<Account,Integer> {
-
-}
